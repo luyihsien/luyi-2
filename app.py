0faabed5f9cb922b818f654a31bb791cd147b222
DESKTOP-P5TVD9N\luyih <luyihsien@gmail.com>
2018-04-21 15:04:19 +0800

    'try'

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=event.message.text+'嗎?'))
 
 
 if __name__ == "__main__":
