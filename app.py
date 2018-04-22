1d07469b56b9cf6fe26489d3d94f0113c202d645
DESKTOP-P5TVD9N\luyih <luyihsien@gmail.com>
2018-04-22 16:19:59 +0800

    'try'

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import random
-import configparser
 from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
@@ -12,15 +10,13 @@ from linebot.models import (
 )
 
 app = Flask(__name__)
-config = configparser.ConfigParser()
-config.read("C:\\Users\\luyih\\Desktop\\linebot1\\config.ini")
+#config.read("C:\\Users\\luyih\\Desktop\\linebot1\\config.ini")
 # Channel Access Token
 line_bot_api = LineBotApi('eiIQIeDuC6GyB/gn1gBHrri96ZdVS7fwTxqaw0eu7NlCtqj7fxxjNjj27wj3LKS0MEoXDKcqMlqlXRJPRu+6KUroP+F5ZUtXEqmsHiZbTHL9QqusZOF8s5Sot61+rj261DO0Ujmi2RJTfGLMFvxIbwdB04t89/1O/w1cDnyilFU=')
 #line_bot_api = LineBotApi(config['line_bot']['Channel_Access_Token'])
 # Channel Secret
 handler = WebhookHandler('3f852273ba31afd703e4116a0a0a37c7')
 #handler = WebhookHandler(config['line_bot']['Channel_Secret'])
-print(config['line_bot']['Channel_Secret'])
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
