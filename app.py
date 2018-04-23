fe58382f1f850128fa169bba07b55e2830b686e0
DESKTOP-P5TVD9N\luyih <luyihsien@gmail.com>
2018-04-23 16:17:45 +0800

    'try'

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage,TemplateSendMessage
-)
+from linebot.models import *
 
 app = Flask(__name__)
 # Channel Access Token
