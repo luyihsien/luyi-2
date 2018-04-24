9db331bcb2e0a1d059540b06cb145d9d3112eab2
DESKTOP-P5TVD9N\luyih <luyihsien@gmail.com>
2018-04-24 14:48:09 +0800

    'try'

--- app.py
+++ app.py
@@ -116,13 +116,13 @@ def handle_message(event):
             )
         )
         line_bot_api.reply_message(event.reply_token, buttons_template)
-        return 0
-    if event.message.text ==guess_number():
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="你猜對了  骰子點數是{0}".format(guess_number())))
-        return 0
-    elif event.message.text !=guess_number():
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="你猜錯了  骰子點數是{0}".format(guess_number())))
-        return 0
+        if event.message.text == guess_number():
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="你猜對了  骰子點數是{0}".format(guess_number())))
+            return 0
+        elif event.message.text != guess_number():
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="你猜錯了  骰子點數是{0}".format(guess_number())))
+            return 0
+
     #else:
     buttons_template = TemplateSendMessage(
         alt_text='目錄 ',
